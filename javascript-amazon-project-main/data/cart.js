ddbf75168997aec21dcf5b349e44a33dc39085ab
madhavjindalwork2009 <madhavjindalwork@gmail.com>
2026-09-06 17:01:22 +0530

    Move addToCart into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId, quantity) {
+	let matchingCartItem;
+
+	cart.forEach((cartItem) => {
+		if (productId === cartItem.productId) {
+			matchingCartItem = cartItem;
+		}
+	});
+
+	if (matchingCartItem) {
+		matchingCartItem.quantity += quantity;
+	} else {
+		cart.push({
+			productId,
+			quantity
+		});
+	}
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 let productsHTML = '';
 
@@ -57,6 +57,19 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+const cartQuantityElement = document.querySelector('.js-cart-quantity');
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  cartQuantityElement.textContent = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
@@ -66,26 +79,7 @@ document.querySelectorAll('.js-add-to-cart')
           .querySelector('select').value
       );
 
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += quantity;
-      } else {
-        cart.push({
-          productId,
-          quantity
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      addToCart(productId, quantity);
+      updateCartQuantity();
     });
   });
